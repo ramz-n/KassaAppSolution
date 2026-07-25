0def1546d67c2ef6f90387fc13da1451aa1a8283
ramz <aly.ranzam@gmail.com>
2026-07-25 09:44:49 +0545

    add cashier entities

---
+++ Kassa.Domain/Entities/Cashier.cs
@@ -0,0 +1,11 @@
+﻿namespace Kassa.Domain.Entities
+{
+    public class Cashier
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string PinCode { get; set; }
+        public bool IsManager { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
