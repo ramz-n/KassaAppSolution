a8528878561020fb81b0b0e6a8abb2dd491938e0
ramz <aly.ranzam@gmail.com>
2026-07-25 18:07:03 +0545

    add a relaycommand

---
+++ Kassa.DesktopApp/Common/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Input;
+
+namespace Kassa.DesktopApp.Common
+{
+    public class RelayCommandAsync : ICommand
+    {
+        private readonly Func<object?, Task> _execute;
+        private readonly Predicate<object?>? _canExecute;
+        private bool _isExecuting;
+
+        public RelayCommandAsync(Func<Task> execute, Func<bool>? canExecute = null)
+            : this(_ => execute(), canExecute is null ? null : _ => canExecute())
+        {
+        }
+
+        public RelayCommandAsync(Func<object?, Task> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+        public async void Execute(object? parameter)
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+            try
+            {
+                await _execute(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+    }
+
+}
