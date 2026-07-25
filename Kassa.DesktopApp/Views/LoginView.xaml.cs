0771e4757d777ad4965c3c668363cb0ee34c4cf2
ramz <aly.ranzam@gmail.com>
2026-07-25 18:06:26 +0545

    add login view

---
+++ Kassa.DesktopApp/Views/LoginView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Kassa.DesktopApp.Views
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void PinBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
