ed6aadd9b7445f9e7edf837d5345eb2d1653e445
ramz <aly.ranzam@gmail.com>
2026-07-26 18:47:54 +0545

    Add products and logout to checkoutview and the related functions

---
+++ Kassa.DesktopApp/Views/CheckoutView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Kassa.DesktopApp.Views
+{
+    public partial class CheckoutView : UserControl
+    {
+        public CheckoutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
