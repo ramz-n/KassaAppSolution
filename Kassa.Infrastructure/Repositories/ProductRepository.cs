e7d40955201ff5c9ddb4a904d056aeed87e22a60
ramz <aly.ranzam@gmail.com>
2026-07-26 18:47:17 +0545

    update product repository interface

--- Kassa.Infrastructure/Repositories/ProductRepository.cs
+++ Kassa.Infrastructure/Repositories/ProductRepository.cs
@@ -17,5 +17,26 @@ namespace Kassa.Infrastructure.Repositories
         {
             return _context.Products.ToListAsync();
         }
+
+        public Task<Product?> GetProductByBarcodeAsync(string barcode)
+        {
+            return _context.Products.FirstOrDefaultAsync(p => p.Barcode == barcode);
+        }
+
+        public Task<List<Product>> SearchProductByNameAsync(string search)
+        {
+            return _context.Products
+                .Where(p => EF.Functions.ILike(p.ProductName, $"%{search}%"))
+                .OrderBy(p => p.ProductName)
+                .ToListAsync();
+        }
+
+        public Task<List<Product>> GetLowStockProductsAsync()
+        {
+            return _context.Products
+                .Where(p => p.StockQty < p.LowStockQty)
+                .OrderBy(p => p.StockQty)
+                .ToListAsync();
+        }
     }
 }
