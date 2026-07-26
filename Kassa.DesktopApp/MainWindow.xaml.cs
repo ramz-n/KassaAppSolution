1e8f335622e1e503f05f639ea17b82d504588f9d
ramz <aly.ranzam@gmail.com>
2026-07-26 18:50:27 +0545

    Add showcheckout and product screen to mainwindow

--- Kassa.DesktopApp/MainWindow.xaml.cs
+++ Kassa.DesktopApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Kassa.DesktopApp.ViewModels;
 using Kassa.DesktopApp.Views;
+using Kassa.Domain.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
@@ -17,8 +18,28 @@ namespace Kassa.DesktopApp
         {
             var vm = App.AppHost.Services.GetRequiredService<LoginViewModel>();
             var view = new LoginView { DataContext = vm };
-            
+            vm.LoginSucceeded += (_, cashier) => ShowCheckoutScreen(cashier);
             MainContent.Content = view;
+        }
+
+        public void ShowCheckoutScreen(Cashier cashier)
+        {
+            var vm = App.AppHost.Services.GetRequiredService<CheckoutViewModel>();
+            vm.Initialize(cashier);
+            var view = new CheckoutView { DataContext = vm };
+            vm.LogoutRequested += (_, _) => ShowLoginScreen();
+            vm.OpenProductsRequested += (_, _) => ShowProductsScreen(cashier);
+
+            MainContent.Content = view;
+        }
+
+        public void ShowProductsScreen(Cashier cashier)
+        {
+            var vm = App.AppHost.Services.GetRequiredService<ProductViewModel>();
+            var view = new ProductView { DataContext = vm };
+            vm.BackRequested += (_, _) => ShowCheckoutScreen(cashier);
+            MainContent.Content = view;
+            _ = vm.LoadAsync();
         }
     }
 }
