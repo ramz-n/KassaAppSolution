1bb6ca7f5973308bb6df9ad612eb1c327915b6f5
ramz <aly.ranzam@gmail.com>
2026-07-28 10:31:55 +0545

    extract interface for kassasession

---
+++ Kassa.Application/Interfaces/IKassaSessionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Kassa.Domain.Entities;
+
+namespace Kassa.Application.Interfaces
+{
+    public interface IKassaSessionRepository
+    {
+        Task<KassaSession?> GetOpenSessionAsync(int cashierId);
+        Task AddAsync(KassaSession session);
+        Task UpdateAsync(KassaSession session);
+    }
+}
