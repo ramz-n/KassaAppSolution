505e5b3f0f3be5433791d7007fcddfdaee9eb8ae
ramz <aly.ranzam@gmail.com>
2026-07-28 10:32:46 +0545

    add kassarepository to open, add and update session for cashier

---
+++ Kassa.Infrastructure/Repositories/KassaSessionRepository.cs
@@ -0,0 +1,33 @@
+﻿using Kassa.Application.Interfaces;
+using Kassa.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kassa.Infrastructure.Repositories
+{
+    public class KassaSessionRepository: IKassaSessionRepository
+    {
+        private readonly AppDbContext _context;
+
+        public KassaSessionRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public Task<KassaSession?> GetOpenSessionAsync(int cashierId)
+        {
+            return _context.KassaSessions.FirstOrDefaultAsync(s => s.CashierId == cashierId && !s.IsClosed);
+        }
+
+        public async Task AddAsync(KassaSession session)
+        { 
+            _context.KassaSessions.Add(session);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(KassaSession session)
+        {
+            _context.KassaSessions.Update(session);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
