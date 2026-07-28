27197be94ecb892852bf852de06c87b5d8fd9e0d
ramz <aly.ranzam@gmail.com>
2026-07-28 10:31:26 +0545

    Add KassSession Entities

---
+++ Kassa.Domain/Entities/KassaSession.cs
@@ -0,0 +1,24 @@
+﻿namespace Kassa.Domain.Entities
+{
+    public class KassaSession
+    {
+        public int Id { get; set; }
+
+        public int CashierId { get; set; }
+        public Cashier? Cashier { get; set; }
+
+        public DateTime OpenedAt { get; set; }
+        public DateTime? ClosedAt { get; set; }
+
+        public decimal StartingCash { get; set; }
+
+        public decimal? ExpectedCash { get; set; }
+
+        public decimal? CountedCash { get; set; }
+
+        public decimal? CashDifference { get; set; }
+
+        public bool IsClosed { get; set; }
+    }
+}
+}
