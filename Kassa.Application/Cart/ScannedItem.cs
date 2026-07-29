6b50589db6e6932b06a987e0f6de7c6057910377
ramz <aly.ranzam@gmail.com>
2026-07-29 18:47:08 +0545

    add scanned items that are pending to be checkout

---
+++ Kassa.Application/Cart/ScannedItem.cs
@@ -0,0 +1,21 @@
+﻿using Kassa.Domain.Enums;
+
+namespace Kassa.Application.Cart
+{
+    public class ScannedItem
+    {
+        public required int ProductId { get; init; }
+        public required string ProductName { get; init; }
+        public required string Barcode { get; init; }
+        public decimal Tax { get; init; }
+        public UnitType UnitType { get; init; }
+        public decimal UnitPrice { get; init; }
+        public decimal Quantity { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+
+        public decimal GrossTotal => Math.Round(Quantity * UnitPrice, 2, MidpointRounding.AwayFromZero);
+
+        public decimal NetTotal => Math.Max(0, GrossTotal - DiscountAmount);
+    }
+}
