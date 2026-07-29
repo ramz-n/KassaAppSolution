fe0f15f9b691bd3eb24ca56525be146989fdfe92
ramz <aly.ranzam@gmail.com>
2026-07-29 10:45:05 +0545

    add localtime to utc and vice-versa convertor

---
+++ Kassa.DesktopApp/Converters/Converters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Kassa.DesktopApp.Converters
+{
+    public class UtcToLocalTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime utcTime)
+            {
+                return utcTime.ToLocalTime();
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime localTime)
+            {
+                return localTime.ToUniversalTime();
+            }
+            return value;
+        }
+    }
+}
