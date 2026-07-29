e9bd2eb36d3833c0bb8cd4f0f6f270d1ad4e29d9
ramz <aly.ranzam@gmail.com>
2026-07-29 18:47:39 +0545

    add viewmodels for scanned item

---
+++ Kassa.DesktopApp/ViewModels/ScannedItemViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Kassa.Application.Cart;
+using Kassa.Domain.Enums;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Kassa.DesktopApp.ViewModels
+{
+    public class ScannedItemViewModel: INotifyPropertyChanged
+    {
+        public ScannedItem Model { get;  }
+        private readonly Action _onChanged;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string ProductName => Model.ProductName;
+        public string Barcode => Model.Barcode;
+        public string UnitLabel => Model.UnitType == UnitType.Weight ? "kg" : "pcs";
+        public decimal NetTotal => Model.NetTotal;
+        public decimal Quantity
+        {
+            get => Model.Quantity;
+            set
+            {
+                if (value <= 0) return;
+                Model.Quantity = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(NetTotal));
+                _onChanged();
+            }
+        }
+
+        public decimal UnitPrice => Model.UnitPrice;
+
+        public decimal DiscountAmount
+        {
+            get => Model.DiscountAmount;
+            set
+            {
+                Model.DiscountAmount = Math.Max(0, Math.Min(value, Model.GrossTotal));
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(NetTotal));
+                _onChanged();
+            }
+        }
+        public ScannedItemViewModel(ScannedItem model, Action onChanged)
+        {
+            Model = model;
+            _onChanged = onChanged;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
