7ddf7648cc4e49a1f2f8e007d7793dd0ba37648e
ramz <aly.ranzam@gmail.com>
2026-07-30 12:05:49 +0545

    add taxcalculator service

---
+++ Kassa.Application/Services/ITaxCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace Kassa.Application.Services
+{
+    public interface ITaxCalculator
+    {
+        decimal CalculateExclusiveAmount(decimal lineTotalIncludingTax, decimal taxRate);
+        decimal CalculateTaxPortion(decimal lineTotalIncludingTax, decimal taxRate);
+    }
+}
---
+++ Kassa.Application/Services/TaxCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Kassa.Application.Services
+{
+    public class TaxCalculator : ITaxCalculator
+    {
+        private const int DecimalPlaces = 2;
+
+        public decimal CalculateTaxPortion(decimal lineTotalIncludingTax, decimal taxRate)
+        {
+            if (taxRate < 0) throw new ArgumentOutOfRangeException(nameof(taxRate));
+            if (taxRate == 0) return 0m;
+
+            var exclusive = CalculateExclusiveAmount(lineTotalIncludingTax, taxRate);
+            var tax = lineTotalIncludingTax - exclusive;
+            return Math.Round(tax, DecimalPlaces, MidpointRounding.AwayFromZero);
+        }
+
+        public decimal CalculateExclusiveAmount(decimal lineTotalIncludingTax, decimal taxRate)
+        {
+            if (taxRate < 0) throw new ArgumentOutOfRangeException(nameof(taxRate));
+            var exclusive = lineTotalIncludingTax / (1 + taxRate);
+            return Math.Round(exclusive, DecimalPlaces, MidpointRounding.AwayFromZero);
+        }
+    }
+}
