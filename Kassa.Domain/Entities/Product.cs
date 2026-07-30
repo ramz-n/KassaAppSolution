5cc614e5966ba66dff70f13c85012e83a5dfe782
ramz <aly.ranzam@gmail.com>
2026-07-30 16:20:29 +0545

    add dataannotation for Product Id

--- Kassa.Domain/Entities/Product.cs
+++ Kassa.Domain/Entities/Product.cs
@@ -1,12 +1,16 @@
 ﻿using Kassa.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Kassa.Domain.Entities
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public required string ProductName { get; set; }
         public required string  Barcode { get; set; }
