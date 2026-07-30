a2ee1a3149d9916bc014f1b19aa462628188740d
ramz <aly.ranzam@gmail.com>
2026-07-30 16:18:55 +0545

    add bool converter

--- Kassa.DesktopApp/Converters/Converters.cs
+++ Kassa.DesktopApp/Converters/Converters.cs
@@ -1,5 +1,7 @@
 ﻿using Kassa.Domain.Entities;
+using Kassa.Domain.Enums;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Kassa.DesktopApp.Converters
@@ -36,5 +38,45 @@ namespace Kassa.DesktopApp.Converters
         {
             throw new NotImplementedException();
         }
+    }
+
+    public class PaymentMethodConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not PaymentMethod current || parameter is not string paramStr) return false;
+            return Enum.TryParse<PaymentMethod>(paramStr, out var target) && current == target;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked && isChecked && parameter is string paramStr &&
+                Enum.TryParse<PaymentMethod>(paramStr, out var target))
+            {
+                return target;
+            }
+            return Binding.DoNothing;
+        }
+    }
+    public class PaymentMethodVisibilityConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            value is PaymentMethod.Cash ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            throw new NotSupportedException();
+    }
+
+    public class BoolToCollapsedConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var flag = value is bool b && b;
+            if (string.Equals(parameter as string, "Inverse", StringComparison.OrdinalIgnoreCase)) flag = !flag;
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            throw new NotSupportedException();
     }
 }
